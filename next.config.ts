c33ceed4e4f6b1b0712c5f33cd033b34b8384972
ilhamnih12 <hamyeu600@gmail.com>
2026-05-18 12:32:00 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  output: 'standalone',
+  output: 'export', // <-- WAJIB diganti ke 'export' ya Ed!
+  images: {
+    unoptimized: true, // <-- Tambahkan ini juga biar gambar aman di mode statis
+  },
   transpilePackages: ['motion'],
   webpack: (config, {dev}) => {
     if (dev && process.env.DISABLE_HMR === 'true') {
